4c71bed1578aee095c395eb6c7d2395bb3918e41
Simon Gerber <simon.gerber@vshn.ch>
2026-06-17 10:27:30 +0200

    Extend the basic controller test case to work with the implementation
    Some latent bugs:
    * Controller doesn't actually treat all optional fields as optional
      (e.g. ingress backend selector).
    * Some hard crashes on misconfigured custom resources

--- internal/controller/loadbalancerconfig_controller_test.go
+++ internal/controller/loadbalancerconfig_controller_test.go
@@ -2,12 +2,16 @@ package controller
 
 import (
 	"context"
+	"net/netip"
+	"os"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"tailscale.com/net/netmon"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -18,11 +22,19 @@ var _ = Describe("LoadBalancerConfig Controller", func() {
 	Context("When reconciling a resource", func() {
 		const resourceName = "test-resource"
 
+		// TODO(sg): what's the right way to
+		configRoot, err := os.MkdirTemp("/tmp", "bootc-lb-test-*")
+		Expect(err).NotTo(HaveOccurred())
+		detectedPublicInterface, err := netmon.DefaultRouteInterface()
+		Expect(err).NotTo(HaveOccurred())
+		clusterNet, err := netip.ParsePrefix("192.168.100.0/24")
+		Expect(err).NotTo(HaveOccurred())
+
 		ctx := context.Background()
 
 		typeNamespacedName := types.NamespacedName{
 			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+			Namespace: "default",
 		}
 		loadbalancerconfig := &configv1alpha1.LoadBalancerConfig{}
 
@@ -30,12 +42,60 @@ var _ = Describe("LoadBalancerConfig Controller", func() {
 			By("creating the custom resource for the Kind LoadBalancerConfig")
 			err := k8sClient.Get(ctx, typeNamespacedName, loadbalancerconfig)
 			if err != nil && errors.IsNotFound(err) {
+				creds := &corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "cloudscale-token",
+						Namespace: "default",
+					},
+					Data: map[string][]byte{
+						"token": []byte("verysecrettoken"),
+					},
+				}
+				Expect(k8sClient.Create(ctx, creds)).To(Succeed())
 				resource := &configv1alpha1.LoadBalancerConfig{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      resourceName,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
+					Spec: configv1alpha1.LoadBalancerConfigSpec{
+						Cloud: "cloudscale",
+						CloudCredentials: corev1.LocalObjectReference{
+							Name: creds.Name,
+						},
+						Distribution: "openshift",
+						VirtualAddresses: configv1alpha1.LoadBalancerConfigVIPs{
+							API: []configv1alpha1.VirtualAddress{
+								{
+									Type:    "public",
+									Address: "198.51.100.100/32",
+								},
+							},
+							Ingress: []configv1alpha1.VirtualAddress{
+								{
+									Type:    "public",
+									Address: "198.51.100.80/32",
+								},
+							},
+							NAT: &configv1alpha1.VirtualAddress{
+								Type:    "public",
+								Address: "198.51.100.253/32",
+							},
+						},
+						APIBackend: configv1alpha1.LoadBalancerConfigBackend{
+							NodeSelector: metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									"node-role.kubernetes.io/control-plane": "",
+								},
+							},
+						},
+						IngressBackend: &configv1alpha1.LoadBalancerConfigBackend{
+							NodeSelector: metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									"node-role.kubernetes.io/infra": "",
+								},
+							},
+						},
+					},
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
@@ -55,6 +115,15 @@ var _ = Describe("LoadBalancerConfig Controller", func() {
 			controllerReconciler := &LoadBalancerConfigReconciler{
 				Client: k8sClient,
 				Scheme: k8sClient.Scheme(),
+
+				ConfigRoot:      configRoot,
+				PublicInterface: detectedPublicInterface,
+				ClusterNetwork:  clusterNet,
+				WatchNamespace:  "default",
+				Hostname:        "lb1",
+				KeepalivedConfig: KeepalivedConfig{
+					IsPrimary: false,
+				},
 			}
 
 			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
